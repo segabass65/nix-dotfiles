8472ea5debdf8cc524ceacadf8988aeb31f5e543
segabass65 <andreypiskunov2008@gmail.com>
2026-03-12 18:58:46 +0300

    refactor(librewolf): migrate settings to librewolf.overrides.cfg

--- home-configs/librewolf/user.js
+++
@@ -1,4 +0,0 @@
-user_pref("full-screen-api.warning.timeout", 0);
-user_pref("privacy.resistFingerprinting", false);
-user_pref("reader.parse-on-load.enabled", false);
-user_pref("webgl.enable-debug-renderer-info", false);
