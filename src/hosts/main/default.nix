816bb178f4b004a3d0c7d07d75e35a5f78557412
segabass65 <andreypiskunov2008@gmail.com>
2026-03-22 01:25:23 +0300

    refactor(nixos): merge content of default.nix into misc.nix

--- src/hosts/main/default.nix
+++ src/hosts/main/default.nix
@@ -1,10 +1,25 @@
-{ ... }: {
+{ hostName, pkgs, ... }: {
   imports = [
     ./hardware.nix
-    ./misc.nix
     ./packages.nix
     ./programs.nix
     ./services.nix
     ./users.nix
   ];
+
+  boot.loader.systemd-boot.enable = true;
+
+  catppuccin.enable = true;
+
+  networking.hostName = hostName;
+  nix.settings.experimental-features = [ "flakes" "nix-command" ];
+  system.stateVersion = "25.11";
+
+  virtualisation.libvirtd = {
+    enable = true;
+    qemu = {
+      swtpm.enable = true;
+      vhostUserPackages = [ pkgs.virtiofsd ];
+    };
+  };
 }
