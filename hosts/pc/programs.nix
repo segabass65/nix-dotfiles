3a3a2d594125a7531dceb1c9a7ed74a2222754db
segabass65 <segabass65@proton.me>
2026-05-11 19:45:01 +0300

    feat(pc/programs.nix): migrate from AmneziaVPN to Throne

--- hosts/pc/programs.nix
+++ hosts/pc/programs.nix
@@ -1,7 +1,10 @@
 { ... }: {
   programs = {
-    amnezia-vpn.enable = true;
     firejail.enable = true;
+    throne = {
+      enable = true;
+      tunMode.enable = true;
+    };
     steam.enable = true;
     virt-manager.enable = true;
     zsh.enable = true;
