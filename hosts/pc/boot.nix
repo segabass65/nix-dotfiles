2577bbf150ccfb70e8101f1f77eaa0ea08f710c5
segabass65 <segabass65@proton.me>
2026-06-22 16:53:16 +0300

    style(pc/boot.nix): add empty lines

--- hosts/pc/boot.nix
+++ hosts/pc/boot.nix
@@ -1,6 +1,7 @@
 { pkgs, ... }: {
   boot = {
     blacklistedKernelModules = [ "nouveau" ];
+
     initrd.availableKernelModules = [
       "nvme"
       "xhci_pci"
@@ -9,6 +10,7 @@
       "usbhid"
       "sd_mod"
     ];
+
     kernelModules = [ "kvm-intel" ];
     kernelPackages = pkgs.linuxPackages_zen;
     loader.systemd-boot.enable = true;
