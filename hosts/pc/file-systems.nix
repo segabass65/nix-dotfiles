bfa022e37db0bd809cfb6aadfb08e84065c859fd
segabass65 <segabass65@proton.me>
2026-07-29 20:21:29 +0300

    feat(hosts/pc/file-systems.nix): update labels and remove unused

--- hosts/pc/file-systems.nix
+++ hosts/pc/file-systems.nix
@@ -1,38 +1,18 @@
 { ... }: {
   fileSystems = {
     "/" = {
-      device = "/dev/disk/by-label/ROOT";
+      device = "/dev/disk/by-label/NIXROOT";
       fsType = "ext4";
     };
 
     "/boot" = {
-      device = "/dev/disk/by-label/BOOT";
+      device = "/dev/disk/by-label/NIXBOOT";
       fsType = "vfat";
       options = [ "fmask=0077" "dmask=0077" ];
     };
 
     "/home" = {
-      device = "/dev/disk/by-label/HOME";
-      fsType = "ext4";
-    };
-
-    "/games" = {
-      device = "/dev/disk/by-label/GAMES";
-      fsType = "ext4";
-    };
-
-    "/data" = {
-      device = "/dev/disk/by-label/DATA";
-      fsType = "ext4";
-    };
-
-    "/vm" = {
-      device = "/dev/disk/by-label/VM";
-      fsType = "ext4";
-    };
-
-    "/repos" = {
-      device = "/dev/disk/by-label/REPOS";
+      device = "/dev/disk/by-label/NIXHOME";
       fsType = "ext4";
     };
   };
