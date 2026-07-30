c51afa9afa5015e8954278e5d029fa93033ceb65
segabass65 <segabass65@proton.me>
2026-07-31 02:51:01 +0300

    feat(pc/segabass65/programs): remove fastfetch config

--- hosts/pc/users/segabass65/programs/default.nix
+++ hosts/pc/users/segabass65/programs/default.nix
@@ -1,12 +1,12 @@
 { ... }: {
   imports = [
-    ./fastfetch.nix
     ./git.nix
     ./nixvim.nix
     ./zsh.nix
   ];
 
   programs = {
+    fastfetch.enable = true;
     gh.enable = true;
     lazygit.enable = true;
     lf.enable = true;
