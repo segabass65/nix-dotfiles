b57bbb3e1710ec0cfb55eacf4f5b79eeb0374086
segabass65 <segabass65@proton.me>
2026-08-05 00:52:35 +0300

    feat(srv/segabass65/programs/default.nix): disable unused

--- hosts/srv/users/segabass65/programs/default.nix
+++ hosts/srv/users/segabass65/programs/default.nix
@@ -6,10 +6,6 @@
   programs = {
     fastfetch.enable = true;
     git.enable = true;
-    home-manager.enable = true;
     neovim.enable = true;
-    rtorrent.enable = true;
-    uv.enable = true;
-    vifm.enable = true;
   };
 }
