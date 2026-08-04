5ab889690005d46f9d71985efd2ed1412fdbf6aa
segabass65 <segabass65@proton.me>
2026-08-05 01:10:04 +0300

    feat(srv/segabass65/programs): add symlink to pc host git module

--- hosts/srv/users/segabass65/programs/default.nix
+++ hosts/srv/users/segabass65/programs/default.nix
@@ -7,7 +7,6 @@
   programs = {
     fastfetch.enable = true;
     gh.enable = true;
-    git.enable = true;
     lf.enable = true;
   };
 }
---
+++ hosts/srv/users/segabass65/programs/git.nix
@@ -0,0 +1,1 @@
+/home/segabass65/Projects/nix-dotfiles/hosts/pc/home-manager/users/segabass65/programs/git.nix
