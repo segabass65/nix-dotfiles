232f07d517d4a55d7d3c5ef40878c40b877a7bca
segabass65 <segabass65@proton.me>
2026-08-04 22:04:28 +0300

    feat(srv/file-systems.nix): update labels

--- hosts/srv/file-systems.nix
+++ hosts/srv/file-systems.nix
@@ -1,16 +1,18 @@
 { ... }: {
   fileSystems = {
     "/" = {
-      device = "/dev/disk/by-label/ROOT";
+      device = "/dev/disk/by-label/NIXROOT";
       fsType = "ext4";
     };
+
     "/boot" = {
-      device = "/dev/disk/by-label/BOOT";
+      device = "/dev/disk/by-label/NIXBOOT";
       fsType = "vfat";
       options = [ "fmask=0077" "dmask=0077" ];
     };
+
     "/home" = {
-      device = "/dev/disk/by-label/HOME";
+      device = "/dev/disk/by-label/NIXHOME";
       fsType = "ext4";
     };
   };
